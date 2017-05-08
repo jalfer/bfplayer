ff2be694a847d3c1c0315c68fba42f09692a8aa0
jalfer <574760305@qq.com>
2017-05-08 13:56:58 +0800

    first commit.

---
+++ app/src/main/java/com/jalfer/bfplayer/zipai_p4/ZipaiP4MainActivity.java
@@ -0,0 +1,15 @@
+package com.jalfer.bfplayer.zipai_p4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jalfer.bfplayer.R;
+
+public class ZipaiP4MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zipai_p4_main);
+    }
+}
